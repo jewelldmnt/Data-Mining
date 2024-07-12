a86d02d5b5698cf17fe62437ee13ca6d7cd564cf
Jewell Anne Diamante <jewellanneddiamante@iskolarngbayan.pup.edu.ph>
2024-07-12 20:28:07 +0800

    drop NaN vals

--- ETL/etl.py
+++ ETL/etl.py
@@ -34,7 +34,9 @@ def transform(telco_df, train_df):
 
 	# Clean and transform data
 	telco_df = transform_telco_data(clean_data(telco_df))
+	telco_df = telco_df.dropna()
 	train_df = transform_train_data(clean_data(train_df))
+	train_df = train_df.dropna()
 
 	# Ensure same columns for both dataframes after transformation
 	telco_df = telco_df.reindex(columns=train_df.columns, fill_value=0)
@@ -42,10 +44,35 @@ def transform(telco_df, train_df):
 	# Merge datasets
 	combined_df = pd.concat([telco_df, train_df], ignore_index=True)
 
+	combined_df = combined_df.dropna()
+	# Check for NaN values after handling them
+	check_nan_values(combined_df, "combined_df after handling NaNs")
+
+	# Count rows in the combined dataframe
+	row_count = combined_df.shape[0]
+	print(f"Total number of rows in the combined dataframe: {row_count}")
+
+	# Check for NaN values before handling them
+	check_nan_values(telco_df, "telco_df after transformation")
+	check_nan_values(train_df, "train_df after transformation")
+	check_nan_values(combined_df, "combined_df before handling NaNs")
+
 	print("Transform Successful")
 	return combined_df
 
-
+def check_nan_values(df, df_name):
+    if df.isnull().values.any():
+        print(f"NaN values found in {df_name}")
+        nan_columns = df.columns[df.isnull().any()].tolist()
+        nan_counts = df.isnull().sum()
+        total_nan = df.isnull().sum().sum()
+        print(f"Columns with NaN values: {nan_columns}")
+        print(f"Count of NaN values per column:\n{nan_counts}")
+        print(f"Total NaN values in {df_name}: {total_nan}")
+        print(f"Rows with NaN values in {df_name}:\n{df[df.isnull().any(axis=1)]}")
+    else:
+        print(f"No NaN values in {df_name}")
+	
 def load(combined_df):
 	print("Load to SQLite Database")
 
